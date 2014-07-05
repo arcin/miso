d4864bae9d932669ed5117bc4ac55c66d78ac1b3
Charles Arciniega <charlesarcin@gmail.com>
2014-07-05 00:08:05 -0700

    Options Object     - Prepare Delete request     - target https://api.github.com/user/repos endpoint

--- gall.js
+++ gall.js
@@ -6,3 +6,11 @@ var https = require('https'),
 var headers = {
   'User-Agent': 'RepoKit/0.0.1'
 };
+
+var options = {
+  hostname: 'api.github.com',
+  path: '/user/repos',
+  auth: githubUser+':'+githubKey,
+  method: 'GET',
+  headers: headers
+};
