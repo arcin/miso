888829044f49bc994d62c760bb5cc2af0248cb16
Charles Arciniega <charlesarcin@gmail.com>
2014-07-04 23:16:21 -0700

    add repoName and github user authentication info

--- gkill.js
+++ gkill.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
-var https = require('https')
+var https = require('https'),
+    repoName = process.argv[2],
+    githubUser = process.env.GITHUB_USERNAME,
+    githubKey = process.env.GITHUB_KEY;
