6ab9d5c9ef86256aad5b3632aaee9563aad33ced
Charles Arciniega <charlesarcin@gmail.com>
2014-07-04 23:19:58 -0700

    prepare headers for options object

--- gkill.js
+++ gkill.js
@@ -3,3 +3,8 @@ var https = require('https'),
     repoName = process.argv[2],
     githubUser = process.env.GITHUB_USERNAME,
     githubKey = process.env.GITHUB_KEY;
+
+var headers = {
+  'Content-Type': 'text/plain',
+  'User-Agent': 'RepoKit/0.0.1'
+};
