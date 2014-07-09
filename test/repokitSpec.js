7a3b0da9143d83ac2690748f2b605a7194101354
Charles Arciniega <charlesarcin@gmail.com>
2014-07-09 01:38:14 -0700

    Tests need to be implemented     - I'm not sure how to test async api calls that log messages to the console.

---
+++ test/repokitSpec.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect,
+    repokit = require('../lib/repokit.js');
+
+describe("repoKit", function(){
+  describe("create", function(){
+    xit("should create a remote repository", function(){
+
+    });
+    xit("should create a local repository", function(){
+
+    });
+    xit("should link the remote and local repositories", function(){
+
+    });
+  });
+
+  describe("destroy", function(){
+    xit("should delete the remote repository", function(){
+
+    });
+  });
+
+  describe("list", function(){
+    xit("should list all public remote repositories", function(){
+
+    });
+  });
+});
